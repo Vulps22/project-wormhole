0c802eeb87e0307bb3f40d93537317a640b571a5
Vulps <ajmcallister5@gmail.com>
2025-07-29 23:10:24 +0100

    Add missile trail

--- GameObjects/Missile.cs
+++ GameObjects/Missile.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace WormholeGame.GameObjects
@@ -14,11 +15,13 @@ namespace WormholeGame.GameObjects
         private double posX;
         private double posY;
         private int bounces;
+        private Queue<Point> trail;
         
         // Missile constants
         public const int DEFAULT_SIZE = 15;
         public const int DEFAULT_SPEED = 6;
         public const int MAX_BOUNCES = 8;
+        public const int TRAIL_LENGTH = 20; // Number of trail points
         
         public Missile(int x, int y, int size, double velX, double velY)
         {
@@ -28,6 +31,10 @@ namespace WormholeGame.GameObjects
             velocityX = velX;
             velocityY = velY;
             bounces = 0;
+            trail = new Queue<Point>();
+            
+            // Initialize trail with starting position
+            trail.Enqueue(new Point(x, y));
         }
         
         public Missile(int x, int y, double velX, double velY) 
@@ -58,6 +65,15 @@ namespace WormholeGame.GameObjects
             
             X = (int)posX;
             Y = (int)posY;
+            
+            // Add current position to trail
+            trail.Enqueue(new Point(X, Y));
+            
+            // Keep trail at max length
+            while (trail.Count > TRAIL_LENGTH)
+            {
+                trail.Dequeue();
+            }
         }
         
         public bool IsExpired()
@@ -67,7 +83,25 @@ namespace WormholeGame.GameObjects
         
         public void Render(Graphics graphics)
         {
-            // Draw missile as red square
+            // Draw trail first (behind the missile)
+            if (trail.Count > 1)
+            {
+                Point[] trailPoints = trail.ToArray();
+                
+                // Draw trail segments with fading alpha
+                for (int i = 0; i < trailPoints.Length - 1; i++)
+                {
+                    // Calculate alpha based on position in trail (newer = more opaque)
+                    int alpha = (int)(255 * ((float)(i + 1) / trailPoints.Length) * 0.6); // Max 60% opacity
+                    
+                    using (Pen trailPen = new Pen(Color.FromArgb(alpha, Color.Red), 15))
+                    {
+                        graphics.DrawLine(trailPen, trailPoints[i], trailPoints[i + 1]);
+                    }
+                }
+            }
+            
+            // Draw missile as red square (on top of trail)
             using (Brush brush = new SolidBrush(Color.Red))
             {
                 graphics.FillRectangle(brush, 
